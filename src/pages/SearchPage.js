b6f92df85f07a15bb96d28774648b19c85fb4311
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-10-31 15:08:14 -0300

    Enhance SearchPage (add description and SearchForm)

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Page, Row } from './styles/Page.style'
+
+import Logo from '../components/logo/Logo'
+import SearchForm from '../components/search/SearchForm'
 
 const mapStateToProps = ({ songs }) => ({
   songs: songs.items
@@ -8,7 +12,17 @@ const mapStateToProps = ({ songs }) => ({
 class SearchPage extends Component {
   render () {
     return (
-      <p>SearchPage</p>
+      <Page>
+        <Row>
+          <Logo />
+        </Row>
+        <Row>
+          Type in something to find songs, albums and artists corresponding to your search.
+        </Row>
+        <Row>
+          <SearchForm />
+        </Row>
+      </Page>
     )
   }
 }
