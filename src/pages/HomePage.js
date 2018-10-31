15c4a1a251baecc184bfd00b86e812f7c22ea9b0
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-10-31 13:35:48 -0300

    Add Row and Col styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Page, Row, Col } from './styles/Page.style'
+
 import Logo from '../components/logo/Logo'
-import { Page } from './styles/Page.style'
+import CTAButton from '../components/button/CTAButton'
 
 class HomePage extends Component {
   render () {
     return (
       <Page>
-        <Logo />
-        <Link to='/search' >To Search</Link>
+        <Row>
+          <Logo />
+        </Row>
+        <Row>
+          <Col>
+            <Link to='/search'>
+              <CTAButton style={{ margin: '0 auto' }}>
+                To Search
+              </CTAButton>
+            </Link>
+          </Col>
+        </Row>
       </Page>
     )
   }
--- src/pages/styles/Page.style.js
+++ src/pages/styles/Page.style.js
@@ -3,5 +3,18 @@ import styled from 'react-emotion'
 export const Page = styled('div')`
   min-height: 100vh;
   text-align: center;
-  padding: 1rem 5%;
+  padding: 0rem 5%;
+`
+
+export const Row = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 1rem 0;
+`
+
+export const Col = styled('div')`
+  flex: 1;
 `
