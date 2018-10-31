67bb31d035f0af8df8f75b6cd44d481c931d055f
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-10-31 13:34:33 -0300

    Enhance Logo style

--- src/components/logo/styles/Logo.style.js
+++ src/components/logo/styles/Logo.style.js
@@ -1,8 +1,7 @@
 import styled from 'react-emotion'
 
 export const LogoImage = styled('img')`
-  width: 100%;
   height: auto;
-  filter: drop-shadow(2px 2px 2px #7552a8);
-  -webkit-filter: drop-shadow(2px 2px 2px #7552a8);
+  width: 100%;
+  max-width: 40rem;
 `
