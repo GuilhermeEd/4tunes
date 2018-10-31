f2e9a82d954de17d7ffae225608d89d46bb1d28a
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-10-31 12:12:04 -0300

    Add Logo component

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import logo from './logo.svg'
+import { LogoImage } from './styles/Logo.style'
+
+const Logo = () => <LogoImage src={logo} alt='Logo' />
+
+export default Logo
---
+++ src/components/logo/styles/Logo.style.js
@@ -0,0 +1,8 @@
+import styled from 'react-emotion'
+
+export const LogoImage = styled('img')`
+  width: 100%;
+  height: auto;
+  filter: drop-shadow(2px 2px 2px #7552a8);
+  -webkit-filter: drop-shadow(2px 2px 2px #7552a8);
+`
